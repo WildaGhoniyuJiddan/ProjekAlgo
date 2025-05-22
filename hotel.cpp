0b02e2ee9cb8743627dcda5ae09f1939f71c2de1
WildaGhoniyuJiddan <ezajidan@gmail.com>
2025-05-22 18:58:54 +0700

    Tambah struct dan sorting harga

--- hotel.cpp
+++ hotel.cpp
@@ -2,6 +2,28 @@
 #include <iomanip>
 using namespace std;
 
+struct Kamar{
+    char nama[50];
+    int nomor;
+    char tipe[50];
+    float bintang;
+    int harga;
+    bool tersedia;
+    Kamar* next;
+};
+
+struct pelanggan{
+    char nama[100];
+    char noTelp[100];
+    char gmail[100];
+    char alamat[100];
+    char namakamar[100];
+    pelanggan* next;
+};
+
+Kamar* head=NULL;
+pelanggan* kepala = nullptr;
+
 void menuLogin();
 void loginAdmin();
 void loginUser();
@@ -118,4 +140,43 @@ void loginAdmin(){
                 break;
         }
     } while(menu!=4);
+}
+
+void sortHarga() {
+    Kamar* arrayHotel[9999]; 
+    int count = 0;
+    Kamar* current = head;
+
+    while (current != nullptr) {
+    arrayHotel[count++] = current;
+    current = current->next;
+    }
+
+for (int i = 0; i < count - 1; i++) {
+    for (int j = 0; j < count - i - 1; j++) {
+        if (arrayHotel[j]->harga > arrayHotel[j + 1]->harga){
+            swap(arrayHotel[j], arrayHotel[j + 1]); //tukar pointer
+        }
+    }
+}
+
+cout << "Data telah diurutkan berdasarkan Harga Termurah:\n";
+    cout << left << setw(56) << setfill ('=') << "" << endl;
+    cout << left 
+        << setw(5) << setfill(' ') << "| No"
+        << setw(20) << setfill(' ') << "| Nama Kamar"
+        << setw(15) << setfill(' ') << "| Harga"
+        << setw(15) << setfill(' ') << "| Status" << "|" << endl;
+    cout << left << setw(56) << setfill ('=') << "" << endl;    
+for (int i = 0; i < count; i++) {
+    string status = (arrayHotel[i]->tersedia == true) ? "Tersedia" : "Terisi";
+    cout << left
+            << setw(2) << setfill(' ') << "|"
+            << setw(3) << setfill(' ') << i+1 << "| "
+            << setw(18) << setfill(' ') << arrayHotel[i]->nama << "| "
+            << setw(13) << setfill(' ') << arrayHotel[i]->harga << "| "
+            << setw(13) << setfill(' ') << status << "|" << endl;
+    }
+    cout << left << setw(56) << setfill ('=') << "" << endl; 
+    system("pause");
 }
